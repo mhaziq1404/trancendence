812c31de45fd0661c365bb5604929328e6b12e05
Muhamad Haziq <haziq@Macs-MacBook-Pro.local>
2024-06-27 11:03:37 +0800

    auto commit by git.sh

---
+++ requirements/front-end/script.js
@@ -0,0 +1,1 @@
+// Your Three.js code goes here
--- requirements/nginx/tools/script.sh
+++ requirements/nginx/tools/script.sh
@@ -1,16 +1,27 @@
-#! bin/bash
+#!/bin/bash
 
-cd /opt && git clone https://github.com/SpiderLabs/ModSecurity
-cd ModSecurity
-git submodule init
-git submodule update
-./build.sh
-./configure
-make
-make install
+cp -r /etc/mount/* /opt/
+mkdir /etc/nginx/modules
+cp /opt/nginx-1.22.1/objs/ngx_http_modsecurity_module.so /etc/nginx/modules/
+rm -rf /usr/share/modsecurity-crs
+git clone https://github.com/coreruleset/coreruleset /usr/local/modsecurity-crs
+mv /usr/local/modsecurity-crs/crs-setup.conf.example /usr/local/modsecurity-crs/crs-setup.conf
+mv /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
 
-cd /opt && sudo git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git
 
-while true; do
-    sleep 10
+mkdir -p /etc/nginx/modsecurity
+cp /opt/ModSecurity/unicode.mapping /etc/nginx/modsecurity
+cp /opt/ModSecurity/modsecurity.conf-recommended /etc/nginx/modsecurity/modsecurity.conf
+
+sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsecurity/modsecurity.conf
+
+echo "\n\n\n script finished"
+
+# Start NGINX
+/usr/sbin/nginx -g 'daemon off;'
+
+while true
+do
+    echo "stuck"
+    sleep 1
 done
