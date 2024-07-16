6978f19e046316aeb6062168e65f3fc08fa4fe53
Muhamad Haziq Muhamad Abdul Hay <mmuhamad@u91z01s02.42kl.edu.my>
2024-07-16 15:31:19 +0800

    commit

--- requirements/back-end/files/ft_transcendence/settings.py
+++ requirements/back-end/files/ft_transcendence/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'ft_transcendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# pip install psycopg2-binary
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pongdb',
-        'USER': 'ponguser',
-        'PASSWORD': 'pongpassword',
-        'HOST': 'database',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# pip install psycopg2-binary
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pongdb',
+#         'USER': 'ponguser',
+#         'PASSWORD': 'pongpassword',
+#         'HOST': 'database',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
